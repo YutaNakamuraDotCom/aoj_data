51694a27a77b41fc2fd97d1da50afff17ad14f53
Yuta Nakamura <gp17a089@oecu.jp>
2020-02-08 16:41:03 +0900

    add mkidls

--- aoj.py
+++ aoj.py
@@ -3,12 +3,13 @@ import json
 import ast
 
 def problem_id():
-    url = 'https://judgeapi.u-aizu.ac.jp/problems?page={page}&size={size}'
+    url = 'https://judgeapi.u-aizu.ac.jp'
+    para = '/problems?page={page}&size={size}'
     savepath = 'problem{num}.json'
 
     for page in list(range(0,1)):
 
-        body = requests.get(url.format(page=page,size=2500))
+        body = requests.get(url + para.format(page=page,size=2500))
 
         a = open(savepath.format(num=page), 'w')
         json.dump(body.json(), a)
@@ -16,9 +17,17 @@ def problem_id():
     return body.json()
 
 def mkidls():
+    url = 'https://judgeapi.u-aizu.ac.jp'
+    para = '/submission_records/problems/{id}?page={page}&size={size}'
+    savepath = 'problem{num}_{page}.json'
     for num in (range(0,2500)):
         a = problem_id()
         b = a[num]
-        print(b['id'])
+        id = b['id']
+        for page in range(0,5):
+            print(page)
+            judgeid = requests.get(url + para.format(id=id,page=page,size=10000))
+            a = open(savepath.format(num=id,page=page), 'w')
+            json.dump(judgeid.json(), a)    
 
 mkidls()
